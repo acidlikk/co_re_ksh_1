c64d059b31b3b7992b29b4ecebdb8bc0b22bb858
acidlikk <acidlikk@gmail.com>
2018-11-01 09:47:01 +0900

    missing file
    Makefile.include
    ADXL355 rf version

---
+++ examples/apros/cc1310/adxl355/node_rf/obj_srf06-cc26xx/announcement.d
@@ -0,0 +1,44 @@
+obj_srf06-cc26xx/announcement.o: \
+ ../../../../../core/net/rime/announcement.c \
+ ../../../../../core/./net/rime/announcement.h \
+ ../../../../../core/./net/linkaddr.h \
+ ../../../../../platform/srf06-cc26xx/./contiki-conf.h project-conf.h \
+ ../../../../../platform/srf06-cc26xx/apros/adxl355/board.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/ioc.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_types.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_chip_def.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_memmap.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_ioc.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_ints.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/interrupt.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_nvic.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/debug.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/cpu.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_cpu_scs.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/rom.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/gpio.h \
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_gpio.h \
+ ../../../../../core/./lib/list.h ../../../../../core/./sys/cc.h \
+ ../../../../../core/./sys/cc-gcc.h
+\ :
+ ../../../../../core/net/rime/announcement.c \ :
+ ../../../../../core/./net/rime/announcement.h \ :
+ ../../../../../core/./net/linkaddr.h \ :
+ ../../../../../platform/srf06-cc26xx/./contiki-conf.h project-conf.h \ :
+ ../../../../../platform/srf06-cc26xx/apros/adxl355/board.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/ioc.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_types.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_chip_def.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_memmap.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_ioc.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_ints.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/interrupt.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_nvic.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/debug.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/cpu.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_cpu_scs.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/rom.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/gpio.h \ :
+ ../../../../../cpu/cc26xx-cc13xx/lib/cc13xxware/inc/hw_gpio.h \ :
+ ../../../../../core/./lib/list.h ../../../../../core/./sys/cc.h \ :
+ ../../../../../core/./sys/cc-gcc.h :
